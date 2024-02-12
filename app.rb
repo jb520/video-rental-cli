c7d92909b394170697d630f341577534d77d1575
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-02-12 17:12:23 +0000

    add list_rented method

---
+++ app.rb
@@ -0,0 +1,4 @@
+require_relative 'video_rental'
+
+video_rental = Video_rental.new
+video_rental.run
--- video_rental.rb
+++ video_rental.rb
@@ -1,6 +1,6 @@
-require_relative "movie"
+require_relative 'movie'
 
-class Video_Rental
+class Video_rental
   def initialize
     @movies = []
     @rented = []
@@ -14,7 +14,8 @@ class Video_Rental
       puts "1. Rent Movie"
       puts "2. Return Movie"
       puts "3. List available Movies"
-      puts "4. Leave"
+      puts "4. List rented Movies"
+      puts "5. Leave"
       print "Enter a number to select an option: "
       option = gets.chomp.to_i
       puts "\n"
@@ -26,6 +27,8 @@ class Video_Rental
       when 3
         list_movies
       when 4
+        list_rented
+      when 5
         puts "See you next time!"
         break
       else
@@ -38,14 +41,32 @@ class Video_Rental
 
   def rent_movie
     #take movie from movies and put it in rented
+    puts "nothing here yet"
   end
 
   def return_movie
     #take movie from rented and put it in movies
+    puts "nothing here yet"
   end
 
   def list_movies
     #list all movies in movies
+    puts "Here's what's available:"
+    @movies.each do |movie|
+      puts movie
+    end
+  end
+
+  def list_rented
+    #list all movies in rented
+    if @rented.length == 0
+      puts "You haven't rented any Movies yet!"
+    else
+      puts "Here's your list:"
+      @rented.each do |movie|
+        puts movie
+      end
+    end
   end
 end
   
