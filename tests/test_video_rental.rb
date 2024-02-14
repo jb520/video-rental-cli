4e67a8221137429a2d9442550d9d57cbf84a0e07
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-02-14 21:58:45 +0000

    add tests

--- tests/test_video_rental.rb
+++ tests/test_video_rental.rb
@@ -1,9 +1,43 @@
 require 'minitest/autorun'
-require '../video_rental'
+require './video_rental'
 
-class TestVideo_rental < Minitest::Test
-  def test_rent
+
+class VideoRentalTest < Minitest::Test
+  def setup
+    @video_rental = Video_rental.new
   end
 
-  def test_return
+  def test_rent_movie_success
+    # Stub user input
+    def @video_rental.gets; "Mission Impossible\n"; end
+    assert_output(/Enjoy your Movie!/) { @video_rental.send(:rent_movie) }
   end
+
+  def test_rent_movie_failure
+    # Stub user input
+    def @video_rental.gets; "Invalid Movie\n"; end
+    assert_output(/I don't think we have that one. :/) { @video_rental.send(:rent_movie) }
+  end
+
+  def test_return_movie_success
+    # Stub user input
+    def @video_rental.gets; "Mission Impossible\n"; end
+    # Rent a movie first
+    @video_rental.send(:rent_movie)
+    assert_output(/has been returned!/) { @video_rental.send(:return_movie) }
+  end
+
+  def test_return_movie_failure
+    # Stub user input
+    def @video_rental.gets; "Invalid Movie\n"; end
+    assert_output(/You haven't rented that movie./) { @video_rental.send(:return_movie) }
+  end
+
+  def test_list_movies
+    assert_output(/Here's what's available:/) { @video_rental.send(:list_movies) }
+  end
+
+  def test_list_rented_empty
+    assert_output(/You haven't rented any Movies yet!/) { @video_rental.send(:list_rented) }
+  end
+end
